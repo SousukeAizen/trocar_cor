a14a955b8d0fe7623845a7eeb3bd56eae97e0a51
SousukeAizen <henriquealves5590@gmail.com>
2026-02-02 13:43:16 -0300

    if e else if para aceitar cores escitas em pt-br

--- main.js
+++ main.js
@@ -3,8 +3,20 @@
 const botaotrocarCor = document.getElementById('trocar-cor')
 
 function trocarCor (){
-    const cor = document.getElementById('cor').value
-    document.documentElement.style.setProperty('--cor-bg', cor)
+    
+    let cor = document.getElementById('cor').value
+    
+    if (cor == 'vermelho') {
+        cor = '#FF0000'
+    } else if (cor == 'azul') {
+        cor = '#0000FF'
+    } else if (cor == 'verde') {
+        cor = '#00AA00'
+    } else if (cor == 'amarelo') {
+        cor = '#FFFF00'
+    }
+        document.documentElement.style.setProperty('--cor-bg', cor)
+    
 }
 
 botaotrocarCor.addEventListener('click', trocarCor )
